fa7ce3cef54b685c14888f269147750ea4d5650d
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-10-15 19:30:07 +0200

    Update lire_fichier_sommets.py
    cast string to int for some value (numero_sommet, ligne, branchement)

--- lire_fichier_sommets.py
+++ lire_fichier_sommets.py
@@ -1,9 +1,11 @@
+
 lst_f = []
 with open("sommets.txt", "r") as filin :
     for ligne in filin:
         lst = ligne.split(";")
-        dico = {"numero_sommet" : lst[0], "nom_sommet" : lst[1], "ligne" : lst[2], "terminus" : lst[3], "branchement" : lst[4]}
+        dico = {"numero_sommet" : int(lst[0]), "nom_sommet" : lst[1], "ligne" : int(lst[2]), "terminus" : lst[3], "branchement" : int(lst[4])}
         lst_f.append(dico)
 
 print(lst_f)
 filin.close()
+
