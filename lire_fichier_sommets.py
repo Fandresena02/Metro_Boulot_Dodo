f366720402cddae123300d2d2f874f1ca80f3326
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-10-22 14:46:39 +0200

    Update lire_fichier_sommets.py
    lecture avec le fichier sommet.txt (commençant par V)

--- lire_fichier_sommets.py
+++ lire_fichier_sommets.py
@@ -2,7 +2,7 @@ def fichier_sommet(nom_fichier):
     lst_f = []
     for ligne in nom_fichier:
         lst = ligne.split(";")
-        dico = {"numero_sommet" : int(lst[0]), "nom_sommet" : lst[1], "ligne" : lst[2], "terminus" : lst[3], "branchement" : int(lst[4])}
+        dico = {"numero_sommet" : int(lst[1]), "nom_sommet" : lst[2], "ligne" : lst[3], "terminus" : lst[4], "branchement" : int(lst[5])}
         lst_f.append(dico)
     return lst_f
 
